2bc0e1015d65c8bacb64ac90e9a03dab10b8c258
youngil <youngil@claion.co.kr>
2025-12-17 09:17:49 +0900

    LoadingMessage 컴포넌트를 추가하고 Counter 및 Profile 컴포넌트를 제거했습니다.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,23 @@
 import { useEffect, useState } from "react";
 
-function Counter() {
-  const [count, setCounter] = useState(0);
+function LoadingMessage() {
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log(`카운트가 ${count}로 변경되었습니다.`);
-  }, [count]);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
 
-  function increase() {
-    setCounter(count + 1);
-  }
+    return () => clearTimeout(timer);
+  }, []);
 
-  return (
-    <div>
-      <h1>카운트 : {count}</h1>
-      <button onClick={increase}>클릭하면 올라가요</button>
-    </div>
-  );
-}
-
-function Profile() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-
-  useEffect(() => {
-    console.log(`이름: ${name}, 나이 ${age}`);
-  }, [name, age]);
-
-  function handleChangeName(event) {
-    setName(event.target.value);
-  }
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="이름을 입력하세요"
-        value={name}
-        onChange={handleChangeName}
-      />
-      <input
-        type="text"
-        placeholder="나이를 입력하세요"
-        value={age}
-        onChange={(event) => setAge(event.target.value)}
-      />
-    </div>
-  );
+  return <div>{loading ? <h1>로딩 중...</h1> : <h1>로딩 완료</h1>}</div>;
 }
 
 function App() {
   return (
     <div>
-      <Counter></Counter>
-      <hr />
-      <Profile></Profile>
+      <LoadingMessage />
     </div>
   );
 }
